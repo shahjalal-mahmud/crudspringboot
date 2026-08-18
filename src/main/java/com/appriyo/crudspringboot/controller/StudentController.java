e55b9e1d4175df7b4d42bb4ed61bbe310cbb02af
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-08-18 18:42:02 +0600

    Implemented Service and Controller file for student

---
+++ src/main/java/com/appriyo/crudspringboot/controller/StudentController.java
@@ -0,0 +1,56 @@
+package com.appriyo.crudspringboot.controller;
+
+import com.appriyo.crudspringboot.entity.Student;
+import com.appriyo.crudspringboot.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/student")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping
+    public ResponseEntity<Student> saveStudent(@RequestBody Student student){
+        Student createStudent = studentService.createStudent(student);
+        return new ResponseEntity<>(createStudent, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable Long id){
+        return studentService.getStudent(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getAllStudents(){
+        List<Student> students = studentService.getAllStudents();
+        return ResponseEntity.ok(students);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable Long id, @RequestBody Student studentDetails){
+        if (!studentService.isExist(id)){
+            return ResponseEntity.notFound().build();
+        }
+        Student updateStudent = studentService.updateStudent(id, studentDetails);
+        return ResponseEntity.ok(updateStudent);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Student> deleteStudent(@PathVariable Long id){
+        if (!studentService.isExist(id)){
+            return ResponseEntity.notFound().build();
+        }
+        studentService.deleteStudent(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/com/appriyo/crudspringboot/service/StudentService.java
+++ src/main/java/com/appriyo/crudspringboot/service/StudentService.java
@@ -10,26 +10,39 @@ import java.util.Optional;
 
 @Service
 public class StudentService {
+
     @Autowired
     private StudentRepository studentRepository;
 
-    private Student creatStudent(Student student){
+    public Student createStudent(Student student) {
         return studentRepository.save(student);
     }
 
-    private Optional<Student> getStudent(Long id){
+    public Optional<Student> getStudent(Long id) {
         return studentRepository.findById(id);
     }
 
-    private List<Student> getALlStudent(){
+    public List<Student> getAllStudents() {
         return studentRepository.findAll();
     }
 
-    private void DeleteStudent(Long id){
+    public void deleteStudent(Long id) {
         studentRepository.deleteById(id);
     }
 
-    private boolean isExist(Long id){
+    public boolean isExist(Long id) {
         return studentRepository.existsById(id);
+    }
+
+    public Student updateStudent(Long id, Student studentDetails){
+        return studentRepository.findById(id)
+                .map(existingStudent -> {
+                    existingStudent.setName(studentDetails.getName());
+                    existingStudent.setAge(studentDetails.getAge());
+                    existingStudent.setRollNumber(studentDetails.getRollNumber());
+                    existingStudent.setSubject(studentDetails.getSubject());
+                    return studentRepository.save(existingStudent);
+                })
+                .orElseThrow( () -> new RuntimeException("Student not found with the id: " + id));
     }
 }
